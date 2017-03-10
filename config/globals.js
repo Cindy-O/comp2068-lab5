d7120a6fb103051dd440a3570678b4a4bb68e90d
Cindy-O <cindy.orbegoso@yahoo.ca>
2017-03-10 16:35:12 -0500

    First commit. Shoult be working

---
+++ config/globals.js
@@ -0,0 +1,9 @@
+/**
+ * Created by RFreeman on 2/8/2017.
+ */
+
+// array of global variables
+module.exports = {
+    // db: 'mongodb://localhost/comp2068-w2017' // local mongodb
+    db: 'mongodb://w2017dba:batman22@ds147599.mlab.com:47599/comp2068'  // mlab
+};
